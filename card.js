d2d957915a514ac6ba50961d7dd8acb1445c9ecc
Viktor Denikin <leshden@yandex.ru>
2022-04-23 22:04:48 +0300

    json

--- card.js
+++ card.js
@@ -1,36 +1,34 @@
-function AddCardContent(element) {
+function AddCardContent(element, content) {
   // let div = document.createElement("div");
   // cardDiv.className = 'card-item-content';
 
-  let title = document.createElement("div");
-  title.className = 'card-item-content-title';
-  title.textContent = 'title';
-  element.appendChild(title);
+  let titleEl = document.createElement("div");
+  titleEl.className = 'card-item-content-title';
+  titleEl.textContent = content.title;
+  element.appendChild(titleEl);
 
-  let img = document.createElement("img");
-  img.className = 'card-item-content-img';
-  img.src = 'https://www.shop.philips.ru/media/catalog/product/cache/23/image/600x/163b81649b7ef7bc8a00b0066e59ae0a/x/d/xd3000_01_image_01.jpg';
-  element.appendChild(img);
+  let imgEl = document.createElement("img");
+  imgEl.className = 'card-item-content-img';
+  imgEl.src = content.image;
+  element.appendChild(imgEl);
 
-  let description = document.createElement("div");
-  description.className = 'card-item-content-description';
-  description.textContent = 'Description';
-  element.appendChild(description);
+  let descriptionEl = document.createElement("div");
+  descriptionEl.className = 'card-item-content-description';
+  descriptionEl.textContent = content.description;
+  element.appendChild(descriptionEl);
 
-  let price = document.createElement("div");
-  price.className = 'card-item-content-price';
-  price.textContent = '1000';
-  element.appendChild(price);
-
-
+  let priceEl = document.createElement("div");
+  priceEl.className = 'card-item-content-price';
+  priceEl.textContent = content.price;
+  element.appendChild(priceEl);
 
   // element.appendChild(div);
 }
 
 
-function AddCard(index) {
+function AddCard(content) {
   let cardDiv = document.createElement("div");
   cardDiv.className = 'card-item';
-  AddCardContent(cardDiv);
+  AddCardContent(cardDiv, content);
   return cardDiv;
 }
--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 document.body.onload = addElement;
 
-const countCards = 4;
-
 function addListItems(element) {
-  for (let i = 0; i < countCards; i++) {
-    element.appendChild(AddCard(i));
-  }
+
+  let req = new XMLHttpRequest();
+
+  req.responseType = 'json';
+  req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+
+      const countCards = req.response.record.length;
+
+      for (let i = 0; i < countCards; i++) {
+        const cardContent = req.response.record[i];
+        element.appendChild(AddCard(cardContent));
+      }
+
+    }
+  };
+
+  req.open("GET", "https://api.jsonbin.io/v3/b/626441cd38be296761f6eb55/latest", true);
+  req.setRequestHeader("X-Master-Key", "$2b$10$348HYOFF3NU3G/Vp0yyV/uK49hb/7tir3La/lOoMafexngUWXfHde");
+  req.send();
 }
 
 function addElement() {
