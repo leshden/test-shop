784fa548d1b650ea6e944584a3a0626099519858
Viktor Denikin <leshden@yandex.ru>
2022-04-25 13:00:32 +0300

    check input data

--- form.js
+++ form.js
@@ -1,7 +1,36 @@
 const closeBtn = document.getElementById("btnClose");
+const submitBtn = document.getElementById("btnSubmit");
+const inputName = document.querySelector(".input-name");
+const inputPhone = document.querySelector(".input-phone");
 
-function onClickCloseForm(){
+function onClickCloseForm() {
   formCard.classList.remove('show-form');
 }
 
+function onSubmitForm() {
+  if (CheckData()) {
+    //отправить
+  }
+}
+
+function CheckData() {
+  if (inputName.value === '') {
+    alert('Имя не добавлено');
+    return false;
+  }
+
+  if (inputPhone.value === '') {
+    alert('Номер пустой');
+    return false;
+  }
+
+  if(!inputPhone.value.match(/^\d{6}$/g)) {
+    alert('Номер должен состоять из 6 цифр');
+    return false;
+  }
+
+  return true;
+}
+
 closeBtn.onclick = onClickCloseForm;
+submitBtn.onclick = onSubmitForm;
